d19962f168c13b5aa945e2397fd37e95e5b65495
Ticoo <JLRGeorge.Lo@gmail.com>
2024-11-16 15:32:12 +0000

    finally backend doesn't return async and twitchio errors

--- backend/backend.py
+++ backend/backend.py
@@ -4,6 +4,7 @@ from flask_cors import CORS
 from dotenv import load_dotenv
 from twitchio.ext import commands
 import threading
+import asyncio
 import json
 
 import logging
@@ -18,54 +19,52 @@ CLIENT_ID = os.getenv('CLIENT_ID')
 CHANNEL_NAME = 'george_f0'
 AUTH_TOKEN = os.getenv('AUTH_TOKEN')
 
-bot = commands.Bot(
-    token=(f"oauth:{os.getenv('AUTH_TOKEN')}"),
-    prefix='!',
-    initial_channels=[CHANNEL_NAME]
-)
-
 message_list = []
 
-@bot.event
-async def event_ready():
-    try:
-        print(f'Logged in as | {bot.nick}', flush=True)
-    except Exception as e:
-        print(f'Error in event_ready: {e}', flush=True)
 
-@bot.event
-async def event_message(message):
-    await bot.process_commands(message)
+class Bot(commands.Bot):
+    def __init__(self):
+        super().__init__(token=(f"oauth:{os.getenv('AUTH_TOKEN')}"), prefix="!", initial_channels=[CHANNEL_NAME])
 
-    name = message.author.name
-    content = message.content
+    async def event_message(self, message):
 
-    message = {
-        "name": name,
-        "content": content
-    }
+        global message_list
+
+        name = message.author.name
+        content = message.content
+
+        message = {
+            "name": name,
+            "content": content
+        }
+        
+        message_list.append(message)
+
+        message_list = message_list[-5:]
     
-    message_list.append(message)
+    async def event_ready(self):
+        try:
+            print(f'Logged in as | {bot.nick}', flush=True)
+        except Exception as e:
+            print(f'Error in event_ready: {e}', flush=True)
 
-    message_list = message_list[-5:]
+
 
 @app.route("/getChat", methods=['GET'])
 def get_chat_messages():
-    return json.dump(message_list)
+    return jsonify(message_list)
 
-def run_bot():
-    bot.run()
 def run_app():
-    app.run(debug=True, use_reloader=False)
+    app.run(port=5000)
 
 if __name__ == '__main__':
-    run_bot()
-    bot_thread = threading.Thread(target=run_bot)
-    bot_thread.start()
-    
 
-    app_thread = threading.Thread(target=run_app)
-    app_thread.start()
+    bot = Bot()
+    flask_thread = threading.Thread(target=run_app)
+    flask_thread.start()
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(bot.run())
 
 
 
